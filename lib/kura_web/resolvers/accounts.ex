5be042c079f91af2355baca347ab8f31a6755f86
Michael Chu <mchchu88@gmail.com>
2022-01-25 01:27:30 -0500

    Adjust ttl for jwt tokens

--- lib/kura_web/resolvers/accounts.ex
+++ lib/kura_web/resolvers/accounts.ex
@@ -4,7 +4,7 @@ defmodule KuraWeb.Resolvers.Accounts do
 
   def sign_up(_parent, args, _context) do
     with {:ok, %User{} = user} <- Accounts.register_user(args),
-         {:ok, jwt, _full_claims} <- Kura.Guardian.encode_and_sign(user) do
+         {:ok, jwt, _full_claims} <- Kura.Guardian.encode_and_sign(user, %{}, ttl: {1, :hour}) do
       {:ok, %{token: jwt}}
     else
       _ -> {:error, "User exists"}
@@ -13,7 +13,7 @@ defmodule KuraWeb.Resolvers.Accounts do
 
   def login(%{email: email, password: password}, _info) do
     with %User{} = user <- Accounts.get_user_by_email_and_password(email, password),
-         {:ok, jwt, _full_claims} <- Kura.Guardian.encode_and_sign(user) do
+         {:ok, jwt, _full_claims} <- Kura.Guardian.encode_and_sign(user, %{}, ttl: {1, :hour}) do
       {:ok, %{token: jwt}}
     else
       _ -> {:error, "Incorrect email or password"}
